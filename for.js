e6a55074e1e1247762e3dc9e82b48d0b46aa4d27
YuiMochiMochi <141051806+YuiMochiMochi@users.noreply.github.com>
2024-05-17 12:16:25 -0300

    add for explicação

---
+++ for.js
@@ -0,0 +1,9 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+// Primera expressão: é executada apenas uma única vez
+// Segunda expressão: consição de execução
+// Terceira expressão: é executada sempre ao final do bloco
+
+for (let indice = 0; indice < numeros.length; indice++) {
+    console.log(numeros[indice]);
+}
